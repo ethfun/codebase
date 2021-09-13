a9f884398bd9785e848cd2a281f98cf01d20d75c
CodinGO <localhost5482436@gmail.com>
2021-09-13 16:02:52 +0800

    to exclude mybatis autoconfig

--- src/main/java/com/inglc/codebase/CodebaseApplication.java
+++ src/main/java/com/inglc/codebase/CodebaseApplication.java
@@ -1,11 +1,14 @@
 package com.inglc.codebase;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @ComponentScan(basePackages = "com.inglc.codebase")
+@EnableAutoConfiguration(exclude = {MybatisAutoConfiguration.class})
 @SpringBootApplication
 public class CodebaseApplication {
 
